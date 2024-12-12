3746d3378806cf60028678f7e70d7a6082a7f6e9
Anastasia Kamysheva <anastasiia.kamysheva@outlook.com>
2024-12-12 11:27:37 +0400

    Add test

--- habithub/users/tests.py
+++ habithub/users/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from habits.models import CustomUser
 
-# Create your tests here.
+class CustomUserTestClass(TestCase):
+    def test_user_creation(self):
+        user = CustomUser.objects.create_user(username='testuser', password='TestPassword')
+        self.assertEqual(user.username, 'testuser')
+        self.assertEqual(user.check_password('TestPassword'))
