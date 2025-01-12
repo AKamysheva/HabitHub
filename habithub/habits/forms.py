dc058311433672fc1ce09099757fc0efcc0c7174
Anastasia Kamysheva <anastasiia.kamysheva@outlook.com>
2025-01-12 11:41:12 +0400

    Some fixes

--- habithub/habits/forms.py
+++ habithub/habits/forms.py
@@ -1,9 +1,5 @@
-from typing import Any, Mapping
 from django import forms
-from django.core.files.base import File
-from django.db.models.base import Model
-from django.forms.utils import ErrorList
-from .models import Habit, Goal
+from .models import Habit, Goal, Remainders
 
 class AddHabitForm(forms.ModelForm):
     class Meta:
@@ -41,9 +37,39 @@ class FeedbackForm(forms.Form):
     feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': '4', 'cols': '40', 'placeholder': 'Ваши предложения по улучшению приложения...'}), label='Обатная связь')
 
 
-        
+class ReminderForm(forms.ModelForm):
+    habit = forms.ModelChoiceField(queryset=Habit.objects.none(), empty_label=None, label='Привычка', disabled=True)
+    reminder_frequency = forms.ChoiceField(choices=Remainders.FREQUENCY_CHOICES, label='Частота напоминаний', required=False)
+    
+    class Meta:
+        model = Remainders
+        fields = ['habit', 'reminder_time', 'reminder_frequency']
+        widgets = {
+            'habit': forms.Select(attrs={}),
+            'reminder_time': forms.TimeInput(attrs={'type': 'time'})
+        }
 
-        
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
+        self.habit = kwargs.pop('habit', None)
+        super().__init__(*args, **kwargs)
+        if self.user:
+            self.fields['habit'].queryset = Habit.objects.filter(user=self.user)
+            if self.habit: 
+                self.fields['habit'].initial = self.habit
 
+    def clean_reminder_time(self):
+        reminder_time = self.cleaned_data['reminder_time']
+        if reminder_time:
+            reminder_time = reminder_time.replace(second=0, microsecond=0)
+        return reminder_time
+
+    def save(self, commit=True): 
+        instance = super().save(commit=False) 
+        if commit: 
+            instance.save() 
+        return instance
+
+    
 
 
